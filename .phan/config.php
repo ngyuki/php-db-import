ba8a0b00611c611bd06eedcbaeed62db3489c262
ngyuki <ngyuki.jp@gmail.com>
2017-08-26 13:05:40 +0900

    Add phan config

---
+++ .phan/config.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'file_list' => [],
+
+    'exclude_file_regex' => '@^vendor/.*/(tests|Tests|test|Test)/@',
+
+    'exclude_file_list' => [],
+
+    'directory_list' => [
+        'src/',
+        'vendor/doctrine/dbal/',
+        'vendor/symfony/console/',
+        'vendor/symfony/yaml/',
+    ],
+
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ],
+];
